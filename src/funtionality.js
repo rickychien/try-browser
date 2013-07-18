f43ed8436b8aa56643e793f68eacd687851dc68e
Ricky Chien <ricky060709@gmail.com>
2013-07-18 15:53:42 +0800

    Removed src/funtionality.js.

--- src/funtionality.js
+++
@@ -1,6 +0,0 @@
-(function() {
-  var para = document.createElement('p');
-  var node = document.createTextNode("This paragraph is awesome!!");
-  para.appendChild(node);
-  document.body.appendChild(para);
-})();
