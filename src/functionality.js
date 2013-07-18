1721de3e80d0f33038a100a996a37c95ad31d144
Ricky Chien <ricky060709@gmail.com>
2013-07-18 15:47:24 +0800

    Initialize commit for simple javascript.

---
+++ src/functionality.js
@@ -0,0 +1,6 @@
+(function() {
+  var para = document.createElement('p');
+  var node = document.createTextNode("This paragraph is awesome");
+  para.appendChild(node);
+  document.body.appendChild(para);
+})();
